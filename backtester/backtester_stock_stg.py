3055f6e185790a2c906a351a5987bd8787fc9055
yuseonho <youseonho@naver.com>
2021-10-25 09:54:34 +0900

    백테 코드 일부 수정

--- backtester/backtester_stock_stg.py
+++ backtester/backtester_stock_stg.py
@@ -114,15 +114,13 @@ class BackTesterStockStg:
         conn.close()
 
     def BuyTerm(self):
-        if type(self.df['현재가'][self.index]) == pd.Series:
+        if type(self.df['현재가'][self.index]) == pd.Series or type(self.df_mt['거래대금순위'][self.index]) == pd.Series:
             return False
-        try:
-            if self.code not in self.df_mt['거래대금순위'][self.index]:
-                self.ccond = 0
-            else:
-                self.ccond += 1
-        except KeyError:
-            return False
+
+        if self.code not in self.df_mt['거래대금순위'][self.index]:
+            self.ccond = 0
+        else:
+            self.ccond += 1
         if self.ccond < self.avgtime + 1:
             return False
 
